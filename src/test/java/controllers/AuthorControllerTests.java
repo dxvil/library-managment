77e5fbeb32604138b2512656ccd00e617a2cf9b2
dxvil <volikeurope@gmail.com>
2023-01-15 13:51:20 +0200

    refactor: author tests

--- src/test/java/controllers/AuthorControllerTests.java
+++ src/test/java/controllers/AuthorControllerTests.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -37,7 +38,9 @@ class AuthorControllerTests {
         System.setOut(standardOut);
     }
 
+
     @Test
+    @Order(1)
     @DisplayName("Add a new author with admin rights")
     void createAuthorAsAdmin() {
         authorController.createAuthor(admin, authorName);
@@ -46,34 +49,43 @@ class AuthorControllerTests {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Add a new author with an user rights")
     void createAuthorAsUser() {
+        
         authorController.createAuthor(user, "Joan Roaling");
+
         assertEquals("You have no rights to create a new author.", 
         outputStreamCaptor.toString().trim());
-        assertEquals(1, authors.size());
+        assertEquals(0, authors.size());
     }
+
     @Test
+    @Order(3)
     @DisplayName("Edit author with admin rights")
     void editAuthorWithAdmin() {
-        authorController.createAuthor(admin, authorName);
+        createAuthorAsAdmin();
+        assertEquals(1, authors.size());
+        
         Author foundedAuthor = authorController.findAuthor(null, authorName);
         String newName = "Angelina Stefanik";
         authorController.editAuthor(admin, foundedAuthor.id, newName);
         
+        
         assertFalse(foundedAuthor == null);
         assertNotEquals(authorName, foundedAuthor.name);
-        authorController.deleteAuthor(admin, newName);
     }
 
     @Test
+    @Order(4)
     @DisplayName("Edit author with user rights")
     void editAuthorWithUser() {
-        authorController.createAuthor(admin, authorName);
         Author foundedAuthor = authorController.findAuthor(null, authorName);
         String newName = "Angelina Stefanik";
+
         authorController.editAuthor(user, foundedAuthor.id, newName);
-        
+
+        assertEquals(1, authors.size());
         assertFalse(foundedAuthor == null);
         assertEquals(authorName, foundedAuthor.name);
         assertEquals("You have no rights to edit an author.", 
@@ -83,6 +95,7 @@ class AuthorControllerTests {
     }
 
     @Test
+    @Order(5)
     @DisplayName("Find an author")
     void findAuthor() {
         Author foundedAuthor = authorController.findAuthor(null, authorName);
@@ -91,6 +104,7 @@ class AuthorControllerTests {
     }
 
     @Test
+    @Order(6)
     @DisplayName("Find an non-exist author")
     void findNonExistAuthor() {
         Author foundedAuthor = authorController.findAuthor(null, "Some name");
@@ -101,6 +115,7 @@ class AuthorControllerTests {
    
 
     @Test
+    @Order(7)
     @DisplayName("Delete author with user rights")
     void deleteAuthorWithUser() {
         authorController.deleteAuthor(user, authorName);
@@ -112,8 +127,10 @@ class AuthorControllerTests {
     }
 
     @Test
+    @Order(8)
     @DisplayName("Delete author with admin rights")
     void deleteAuthorWithAdmin() {
+
         authorController.deleteAuthor(admin, authorName);
        
         assertEquals(0, authors.size());
